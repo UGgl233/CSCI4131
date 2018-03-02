c764ff37e95dce002d9a0332a6452b6f27d95d20
Bangyi Yang <yang4820>
2018-03-02 13:25:26 -0600

    Done mostly

--- HW Assignment 4, Due Date Friday March 2nd at 1155 PM-20180226/yang4820_server.py
+++ HW Assignment 4, Due Date Friday March 2nd at 1155 PM-20180226/yang4820_server.py
@@ -18,10 +18,15 @@ import stat
 import sys
 import urllib.parse
 import datetime
+import re
 
+from datetime import datetime
+from urllib.request import urlopen # To open csumn web page
 from threading import Thread
 from argparse import ArgumentParser
 
+
+# Calendar.html !!!!
 
 
 BUFSIZE = 4096
@@ -31,12 +36,16 @@ METHOD_NOT_ALLOWED = 'HTTP/1.1 405  METHOD NOT ALLOWED{}Allow: GET, HEAD{}Connec
 OK = 'HTTP/1.1 200 OK{}{}{}'.format(CRLF, CRLF, CRLF)
 NOT_FOUND = 'HTTP/1.1 404 NOT FOUND{}Connection: close{}{}'.format(CRLF, CRLF, CRLF)
 FORBIDDEN = 'HTTP/1.1 403 FORBIDDEN{}Connection: close{}{}'.format(CRLF, CRLF, CRLF)
-MOVED_PERMANENTLY = 'HTTP/1.1 301 MOVED PERMANENTLY{}Location:  https://www.cs.umn.edu/{}Connection: close{}{}'.format(CRLF, CRLF, CRLF, CRLF)
+MOVED_PERMANENTLY = 'HTTP/1.1 301 MOVED PERMANENTLY{}Location:  https://www.cs.umn.edu/{}Connection: close{}{}'.format(CRLF,CRLF,CRLF,CRLF)
 
 def get_contents(fname):
     with open(fname, 'r') as f:
         return f.read()
 
+# Take %3A token off
+def unquote(time):
+    x = re.split('%3A', time)
+    return (x[0] + ":" + x[1])
 
 def check_perms(resource):
     """Returns True if resource has read permissions set on 'others'"""
@@ -99,17 +108,155 @@ class HTTP_HeadServer:  #A re-worked version of EchoServer
 
   def process_request(self, request):
     print('######\nREQUEST:\n{}######'.format(request))
+
     linelist = request.strip().split(CRLF)
     reqline = linelist[0]
     rlwords = reqline.split()
+    lastline = linelist[-1] # Get the last line of request
+
     if len(rlwords) == 0:
         return ''
 
+    # All supported methods
     if rlwords[0] == 'HEAD':
         resource = rlwords[1][1:] # skip beginning /
         return self.head_request(resource)
+
+    if rlwords[0] == 'GET':
+        resource = rlwords[1][1:]
+        return self.get_request(resource)
+
+    if rlwords[0] == 'POST':
+        return self.post_request(lastline)
+
+    # Bonus
+    if rlwords[0] == 'DELETE':
+        resource = rlwords[1][1:]
+        return self.delete_request(resource)
+
+    if rlwords[0] == 'OPTIONS':
+        resource = rlwords[1][1:]
+        return self.option_request(resource)
+
+    # Bonus
+    if rlwords[0] == 'PUT':
+        resource = rlwords[1][1:]
+        return self.put_request(resource, lastline)
+
     else:
         return METHOD_NOT_ALLOWED
+
+  # def put_request(self, resource, lastline):
+  #     """PUT GET requests."""
+  #     path = os.path.join('.', resource)
+  #
+  #     if not os.path.exists(resource):
+  #         f = open(resource, 'w+')
+  #         f.write(lastline)
+  #         f.close()
+  #
+  #     else:
+
+
+  def post_request(self, lastline):
+      """POST GET requests."""
+      attributes = lastline.split('&')
+      placename = attributes[0].split('=')[1]
+      placename = placename.replace('+', ' ')
+      add1 = attributes[1].split('=')[1]
+      add1 = add1.replace('+', ' ')
+      add2 = attributes[2].split('=')[1]
+      add2 = add2.replace('+', ' ')
+      opentime = attributes[3].split('=')[1]
+      opentime = unquote(opentime)
+      closetime = attributes[4].split('=')[1]
+      closetime = unquote(closetime)
+      additionalinfo = attributes[5].split('=')[1]
+      additionalinfo = additionalinfo.replace('+', ' ')
+      urlinfo = urlinfo[6].split('=')[1]
+      body = '<html><body>' + '<p>Following Form Data Submitted Successfully</p>'\
+      + '<p>Place Name: ' + placename + '</p>'\
+      + '<p>Address Line1: ' + add1 + '</p>'\
+      + '<p>Address Line2: ' + add2 + '</p>'\
+      + '<p>Open Time: ' + opentime + '</p>'\
+      + '<p>Close Time: ' + closetime + '</p>'\
+      + '<p>Additional Info: ' + additionalinfo + '</p>'\
+      + '<p>URL: ' + urlinfo + '</p>'\
+      + '</html></body>'
+
+      return OK + body
+
+  def option_request(self, resource):
+      """Option GET requests."""
+      path = os.path.join('.', resource)
+      header = ''
+      if resource == '':
+          header += 'HTTP/1.1 200 OK{}'.format(CRLF)
+          header += 'Allow: OPTIONS, GET, HEAD, POST, PUT, DELETE\n'
+          header += 'Cache-Control: max-age=604800\n'
+          header += 'Date: ' + str(datetime.now()) + '\n'
+          header += 'Content-Length: 0\n'
+
+      elif resource == 'calendar.html':
+          header += 'HTTP/1.1 200 OK{}'.format(CRLF)
+          header += 'Allow: OPTIONS, GET, HEAD\n'
+          header += 'Cache-Control: max-age=604800\n'
+          header += 'Date: ' + str(datetime.now()) + '\n'
+          header += 'Content-Length: 0\n'
+
+      elif resource == 'form.html':
+          header += 'HTTP/1.1 200 OK{}'.format(CRLF)
+          header += 'Allow: OPTIONS, GET, HEAD, POST\n'
+          header += 'Cache-Control: max-age=604800\n'
+          header += 'Date: ' + str(datetime.now()) + '\n'
+          header += 'Content-Length: 0\n'
+
+      return header
+
+  def delete_request(self, resource):
+      """Delete GET requests."""
+      path = os.path.join('.', resource)
+      if resource == 'csumn':
+          header = MOVED_PERMANENTLY
+          return header
+
+      elif not os.path.exists(resource):
+          header = NOT_FOUND
+          body = get_contents('404.html')
+
+      elif not check_perms(resource):
+          header = FORBIDDEN
+          body = get_contents('403.html')
+
+      else:
+          os.remove(resource)
+          header += 'HTTP/1.1 200 OK{}'.format(CRLF)
+          header += 'Date: ' + str(datetime.now()) + '\n'
+          body = ''
+
+      return (header + body)
+
+  def get_request(self, resource):
+      """Handles GET requests."""
+      path = os.path.join('.', resource)
+      if resource == 'csumn':
+          header = MOVED_PERMANENTLY
+          return header
+
+      elif not os.path.exists(resource):
+          header = NOT_FOUND
+          body = get_contents('404.html')
+
+      elif not check_perms(resource):
+          header = FORBIDDEN
+          body = get_contents('403.html')
+
+      else:
+          header = OK
+          body = get_contents(resource)
+
+      return (header + body)
+
 
   def head_request(self, resource):
     """Handles HEAD requests."""
@@ -128,14 +275,17 @@ class HTTP_HeadServer:  #A re-worked version of EchoServer
 #(you should check types of accept lines before doing so)
 
 def parse_args():
-  parser = ArgumentParser()
-  parser.add_argument('--host', type=str, default='localhost',
-                      help='specify a host to operate on (default: localhost)')
-  parser.add_argument('integers', type=int, default=9001,
-                      help='specify a port to operate on (default: 9001)')
-  args = parser.parse_args()
-  return (args.host, args.integers)
+  # parser = ArgumentParser()
+  # parser.add_argument('--host', type=str, default='localhost',
+  #                     help='specify a host to operate on (default: localhost)')
+  # parser.add_argument('-p', '--port', type=int, default=9001,
+  #                     help='specify a port to operate on (default: 9001)')
+  # args = parser.parse_args()
+  # return (args.host, args.port)
 
+  if (len(sys.argv) == 1):
+      return ('localhost', 9001)
+  return ('localhost', int(sys.argv[1]))
 
 if __name__ == '__main__':
   (host, port) = parse_args()
